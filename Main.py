323ed21fef592d0be677e06bb596a20f97d32a3d
Guillermo-Nacusi <132790670+Guillermo-Nacusi@users.noreply.github.com>
2023-05-07 21:55:15 -0300

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,6 @@
+from ClaseViajeroFrecuente import ViajeroFrecuente
+from Manejador import ManejadorViajero
+if __name__ == "__main__":
+    maV= ManejadorViajero()
+    maV.cargar()
+    maV.funcion123()
